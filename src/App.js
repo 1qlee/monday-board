9911788425bd521190415910d8fbaa298a6d3685
unknown <wonq33@gmail.com>
2022-12-30 16:17:27 -0500

    Fleshing out inputs

--- src/App.js
+++ src/App.js
@@ -12,21 +12,31 @@ const monday = mondaySdk();
 
 const App = () => {
   const [values, setValues] = useState([
-    { title: "Name", type: "text", id: "name" },
-    { title: "Price", type: "numeric", id: "price" },
+    { title: "Name", type: "text", id: "name", text: "Dummy text", value: "name" },
+    { title: "Price", type: "numeric", id: "price", text: "$0", value: "price" },
   ])
   const [jobNum, setJobNum] = useState("")
   const [boardId, setBoardId] = useState(3715125693)
+  const [jobDetails, setJobDetails] = useState({
+    name: {
+      title: "Name", type: "text", id: "name", text: "Dummy text", value: "name",
+    },
+    priority1: {
+      title: "Priority", type: "text", id: "name", text: "Dummy text", value: '{"done_colors":[1],"color_mapping":{"2":10,"7":15,"9":109,"10":19,"15":7,"19":110,"109":9,"110":2},"labels":{"5":"None","7":"Low","109":"Medium","110":"High"},"labels_positions_v2":{"5":3,"7":2,"109":1,"110":0},"labels_colors":{"5":{"color":"#c4c4c4","border":"#B0B0B0","var_name":"grey"},"7":{"color":"#9CD326","border":"#89B921","var_name":"lime - green"},"109":{"color":"#FFCB00","border":"#C0AB1B","var_name":"yellow"},"110":{"color":"#e2445c","border":"#CE3048","var_name":"red - shadow"}}}',
+    },
+  })
   const colTypes = new Set(["name", "board-relation", "multiple-person", "color", "date", "text", "long-text", "numeric", "dropdown"]);
 
   useEffect(() => {
     monday.execute("valueCreatedForUser");
 
+    // get the current boardId from Monday
     monday.get("context").then(res => {
       console.log(res.data)
       setBoardId(res.data.boardId || 3715125693);
       const columnsQuery = `query { boards (ids: ${boardId}) { columns { title type id settings_str }}}`;
 
+      // get all columns and filter them for inputtable fields
       monday.api(columnsQuery).then(res => {
         console.log(res.data.boards)
         const columns = res.data.boards[0].columns
@@ -34,6 +44,7 @@ const App = () => {
 
         if (filteredColumns) {
           setValues(filteredColumns)
+          setJobDetails(arrayToObj(filteredColumns))
         } 
       });
     }).catch(err => {
@@ -41,9 +52,10 @@ const App = () => {
     });
   }, []);
 
+  // retrieve a specific job (an item in Monday)
   const getJob = () => {
     if (jobNum) {
-      const jobQuery = `query { boards (ids: ${boardId}) { items(ids: ${jobNum}) { name column_values { text type title value }}}}`;
+      const jobQuery = `query { boards (ids: ${boardId}) { items(ids: ${jobNum}) { name column_values { text type title value id }}}}`;
   
       // returns an array of all column values for the specified job aka item
       monday.api(jobQuery).then(res => {
@@ -57,16 +69,33 @@ const App = () => {
           text: name,
           title: "Name",
           type: "name",
+          id: "name",
         })
 
         if (filteredColumns) {
           setValues(filteredColumns)
-        } 
+          setJobDetails(arrayToObj(filteredColumns))
+        }
       });
     }
     else {
       console.error("Need job number!")
     }
+  }
+
+  const arrayToObj = array => {
+    const arrayLength = array.length
+    const objectDummy = {}
+
+    for (let i = 0; i < arrayLength; i++) {
+      objectDummy[array[i].id] = array[i]
+    }
+
+    return objectDummy
+  }
+
+  const saveJob = () => {
+
   }
 
   return (
@@ -78,7 +107,6 @@ const App = () => {
           onChange={setJobNum}
           onKeyDown={e => e.key === "Enter" && getJob()}
           placeholder="Job number"
-          trim={true}
         />
         <Button
           onClick={() => getJob()}
@@ -92,19 +120,22 @@ const App = () => {
         >
           {values.map(val => (
             <fieldset>
-              <label
-                htmlFor={val.id}
-              >
-                {val.title}
-              </label>
+              <label htmlFor={val.id}>{val.title}</label>
               <Input 
-                inputProps={val}
+                input={val}
+                jobDetails={jobDetails}
+                setJobDetails={setJobDetails}
               />
             </fieldset>
           ))}
         </Flex>
       </form>
-      <Button>Save</Button>
+      <Button
+        type="submit"
+        name=""
+      >
+        Save
+      </Button>
     </div>
   );
 };
--- src/Input.js
+++ src/Input.js
@@ -1,53 +1,42 @@
 import React, { useMemo, useCallback } from "react";
-import { Flex, TextField, Button, Label, Dropdown } from "monday-ui-react-core"
+import { TextField, Label, Dropdown } from "monday-ui-react-core"
 
-const Input = props => {
-  const generateInput = input => {
-    switch (input.type) {
-      case "name":
-        return (
-          <TextField
-            id={input.id}
-            value={input.text}
-          />
-        )
-      case "board-relation":
-        return (
-          <div>board relation dropdown</div>
-        )
-      case "multiple-person":
-        return (
-          <div>multiple person dropdown</div>
-        )
-      case "color":
+const Input = ({ input, jobDetails, setJobDetails }) => {
 
-      case "date":
-        return (
-          <div>date picker</div>
-        )
-      case "long-text":
-        return (
-          <textarea />
-        )
-      case "numeric":
-        return (
-          <TextField
-            id={input.id}
-          />
-        )
-      case "dropdown":
-        return (
-          <div>dropdown picker</div>
-        )
-      case "subtasks":
-        return null
-      default:
-
-    }
+  const changeInputValue = value => {
+    setJobDetails({
+      ...jobDetails,
+      [input.id]: value,
+    })
   }
 
   return (
-    
+    <>
+      {input.type === "name" && (
+        <TextField
+          required
+          onChange={changeInputValue}
+          value={jobDetails[input.id].text}
+          placeholder="Enter a project name"
+          id={input.id}
+        />
+      )}
+      {input.type === "text" && (
+        <TextField
+          id={input.id}
+        />
+      )}
+      {input.type === "long-text" && (
+        <textarea
+          id={input.id}
+        />
+      )}
+      {input.type === "numeric" && (
+        <TextField 
+          id={input.id}
+        />
+      )}
+    </>
   )
 }
 
